c39d9442e30b8359545c3263c72aa5d3180a7e3d
Dima Skopintsev <skopincev2015@ukr.net>
2016-11-30 22:43:02 +0200

    RecyclerView added from Bionic Lesson

---
+++ app/src/main/java/clicknote/main/article/ArticleDetailsActivity.java
@@ -0,0 +1,29 @@
+package clicknote.main.article;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import clicknote.main.R;
+
+
+/**  */
+public class ArticleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_fragment);
+
+        boolean twoPanel = getResources().getBoolean(R.bool.two_panel_mode);
+        if (twoPanel) {
+            finish();
+            return;
+        } else if (savedInstanceState == null) {
+            final ArticleDetailsFragment f = new ArticleDetailsFragment();
+            f.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.container, f).commit();
+        }
+
+    }
+}
---
+++ app/src/main/java/clicknote/main/article/ArticleListActivity.java
@@ -0,0 +1,22 @@
+package clicknote.main.article;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import clicknote.main.R;
+
+
+/**  */
+public class ArticleListActivity extends AppCompatActivity {
+
+    private static final String LIST_TAG = "article_list";
+    private static final String DETAILS_TAG = "article_details";
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_list);
+    }
+
+}
